45187a101891a4ded15b24e62d43dde340f6ad02
wuxiao2020 <wuxiao2020@163.com>
2018-10-31 09:31:34 +0800

    更新样式

---
+++ fuligou/src/main/java/tech/dao/LabelMapper.java
@@ -0,0 +1,17 @@
+package tech.dao;
+
+import tech.model.Label;
+
+public interface LabelMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Label record);
+
+    int insertSelective(Label record);
+
+    Label selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Label record);
+
+    int updateByPrimaryKey(Label record);
+}
